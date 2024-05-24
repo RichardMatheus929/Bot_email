bc4c79f91bae5463b67b15c12386d91f16e81e1f
Richard Matheus <89545652+RichardMatheus929@users.noreply.github.com>
2024-05-24 08:50:53 -0300

    update conteudo_padrao

--- codigo/Extract.py
+++ codigo/Extract.py
@@ -46,23 +46,5 @@ class Extract:
         return "Nenhuma despesa encontrado"
     def get_conteudo_padrao(self):
         stringa = f"""
-        <p>Segue Ordem de Compra, referente ao {self.get_Nprocesso()}, {self.get_despesa()}, {self.get_pregao()} do município de Touros/RN.</p>
-
-        <p>Observações importantes:</p>
-
-        <p>A Nota Fiscal deve ser faturada conforme a Ordem de Compra.
-        Caso a empresa não disponha da totalidade dos itens para entrega imediata, a Nota Fiscal poderá ser faturada com os itens disponíveis, sendo necessário a edição da Ordem de Compra por parte do setor de compras da prefeitura. Ou seja, os itens não atendidos nesta Nota, podem ser enviados em outra Nota Fiscal, sendo emitida outra Ordem de Compra para a referida Nota.</p>
-
-        <p>Informo que a entrega deverá ser feita das 7h às 13h, de segunda à sexta, no seguinte endereço:
-        CENTRAL DE ABASTECIMENTO FARMACÊUTICO E ALMOXARIFADO Av. 27 de Março, S/N, Centro - Touros/RN por trás do Hospital Municipal Ministro Paulo de Almeida Machado</p>
-
-        <p>Em relação à validade, observar a cláusula do Termo de Referência:
-        "4. CRITÉRIOS DE ACEITAÇÃO DO OBJETO 4.1. A entrega dos produtos deverá ser realizada de segunda à sexta no horário dás 7h as 13h, na Central de Abastecimento Farmacêutico; 4.2. No caso de produtos perecíveis, o prazo de validade na data da entrega não poderá ser inferior a 12 (doze) meses do prazo total recomendado pelo fabricante"</p>
-
-        <p>Solicito que ao emitir a nota fiscal, envie para os seguintes e-mails:
-        pmt.financeiro.rn@gmail.com touroscontroladoria@gmail.com caftouros@gmail.com</p>
-
-        <p>Estou à disposição para quaisquer dúvidas neste e-mail ou contato telefônico citado abaixo. Favor acusar recebimento.</p>
-
-        <p>Atenciosamente,</p>"""
+        """
         return stringa
